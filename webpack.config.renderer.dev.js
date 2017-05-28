f61b6d92eb0f055c9f6ca67b423531025009c9f2
Amila Welihinda <amilajack@gmail.com>
2017-05-27 20:57:04 -0700

    Reverted flow-runtime changes, moved react-hot-loader/babel

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -57,11 +57,9 @@ export default merge.smart(baseConfig, {
           options: {
             cacheDirectory: true,
             plugins: [
-              'react-hot-loader/babel',
-              ['flow-runtime', {
-                assert: true,
-                annotate: true
-              }]
+              // Here, we include babel plugins that are only required for the
+              // renderer process.
+              'react-hot-loader/babel'
             ],
           }
         }
